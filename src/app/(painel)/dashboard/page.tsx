0834f9a14960ce508f0d48a65e67787c382195bf
Matheus <matheus_lopes151@hotmail.com>
2025-10-07 18:59:25 -0300

    continuação sidebasr

--- src/app/(painel)/dashboard/page.tsx
+++ src/app/(painel)/dashboard/page.tsx
@@ -3,6 +3,9 @@ export default function Dashboard() {
     return (
         <div>
             <h1> Dashboard</h1>
+            <div className="w-full h-[600px] bg-gray-200 mb-10"> </div>
+            <div className="w-full h-[600px] bg-gray-200 mb-10"> </div>
+            <div className="w-full h-[600px] bg-gray-200 mb-10"> </div>
         </div >
     )
 }
